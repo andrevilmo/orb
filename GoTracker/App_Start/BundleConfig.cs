c6e3910c9633f7a229d53667b01187fd66775b7e
acardoso <a.cardoso@controlexpert.com>
2014-08-19 13:02:22 -0300

    Initial commit

---
+++ GoTracker/App_Start/BundleConfig.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Web;
+using System.Web.Optimization;
+
+namespace GoTracker
+{
+    public class BundleConfig
+    {
+        public static void AddDefaultIgnorePatterns(IgnoreList ignoreList)
+        {
+            if (ignoreList == null)
+                throw new ArgumentNullException("ignoreList");
+            ignoreList.Ignore("*.intellisense.js");
+            ignoreList.Ignore("*-vsdoc.js");
+            ignoreList.Ignore("*.debug.js", OptimizationMode.WhenEnabled);
+            //ignoreList.Ignore("*.min.js", OptimizationMode.WhenDisabled);
+            ignoreList.Ignore("*.min.css", OptimizationMode.WhenDisabled);
+        }
+
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.IgnoreList.Clear();
+            AddDefaultIgnorePatterns(bundles.IgnoreList);
+            
+            //Styles
+
+            bundles.Add(new StyleBundle("~/Template/Basic/css").Include(
+                "~/Content/Template/css/styles.css"));
+
+
+            //Javascripts
+
+            //Basic Scripts
+            bundles.Add(new ScriptBundle("~/Template/Basic/js").Include(
+                "~/Content/Template/js/vendors/modernizr/modernizr.custom.js"));
+
+             //Jquery
+            bundles.Add(new ScriptBundle("~/Template/JQuery").Include(
+                "~/Content/Template/js/vendors/jquery/jquery.min.js"));
+
+            //JqueryUi
+            bundles.Add(new ScriptBundle("~/Template/JQueryUi").Include(
+                
+                "~/Content/Template/js/vendors/jquery/jquery-ui.min.js",
+                "~/Content/Template/js/vendors/touch-punch/jquery.ui.touch-punch.min.js"));
+
+            //Fullscreen
+            bundles.Add(new ScriptBundle("~/Template/Fullscreen").Include(
+                "~/Content/Template/js/vendors/fullscreen/screenfull.min.js"));
+
+            //Nanoscroller
+            bundles.Add(new ScriptBundle("~/Template/Nanoscroller").Include(
+                "~/Content/Template/js/vendors/nanoscroller/jquery.nanoscroller.min.map",
+                "~/Content/Template/js/vendors/nanoscroller/jquery.nanoscroller.min.js"));
+
+            //Sparkline
+            bundles.Add(new ScriptBundle("~/Template/Sparkline").Include(
+                "~/Content/Template/js/vendors/sparkline/jquery.sparkline.min.js"));
+
+            //HorizontalDropdown
+            bundles.Add(new ScriptBundle("~/Template/HorizontalDropdown").Include(
+                "~/Content/Template/js/vendors/horisontal/cbpHorizontalSlideOutMenu.js", 
+                "~/Content/Template/js/vendors/classie/classie.js"));
+
+            //PowerWidgets
+            bundles.Add(new ScriptBundle("~/Template/PowerWidgets").Include(
+                "~/Content/Template/js/vendors/powerwidgets/powerwidgets.js"));
+
+            //FlotChart
+            bundles.Add(new ScriptBundle("~/Template/FlotChart").Include(
+                "~/Content/Template/js/vendors/flotchart/jquery.flot.min.js", 
+                "~/Content/Template/js/vendors/flotchart/jquery.flot.stack.min.js", 
+                "~/Content/Template/js/vendors/flotchart/jquery.flot.categories.min.js", 
+                "~/Content/Template/js/vendors/flotchart/jquery.flot.time.min.js", 
+                "~/Content/Template/js/vendors/flotchart/jquery.flot.resize.min.js", 
+                "~/Content/Template/js/vendors/flotchart/jquery.flot.axislabels.js", 
+                "~/Content/Template/js/vendors/flotchart/jquery.flot-tooltip.js", 
+                "~/Content/Template/js/vendors/flotchart/jquery.flot.pie.min.js"));
+
+            //Bootstrap
+            bundles.Add(new ScriptBundle("~/Template/Bootstrap").Include(
+                "~/Content/Template/js/vendors/bootstrap/bootstrap.min.js"));
+
+            //ToDo
+            bundles.Add(new ScriptBundle("~/Template/ToDo").Include(
+                "~/Content/Template/js/vendors/todos/todos.js"));
+
+            //MainApp
+            bundles.Add(new ScriptBundle("~/Template/MainApp").Include(
+                "~/Content/Template/js/scripts.js"));
+
+            //Google Maps
+            bundles.Add(new ScriptBundle("~/Template/GoogleMaps").Include(
+               "~/Content/Template/js/vendors/gmap/jquery.gmap.min.js"));
+                
+            // Forms
+             bundles.Add(new ScriptBundle("~/Template/Forms").Include(
+            "~/Content/Template/js/vendors/forms/jquery.form.min.js",
+            "~/Content/Template/js/vendors/forms/jquery.validate.min.js",
+            "~/Content/Template/js/vendors/forms/jquery.maskedinput.min.js",
+            "~/Content/Template/js/vendors/jquery-steps/jquery.steps.min.js"));
+        }
+    }
+}
