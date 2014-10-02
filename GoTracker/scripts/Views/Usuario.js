664f9128b149710a3712edb2d110013f49008b44
acardoso <a.cardoso@controlexpert.com>
2014-10-02 03:33:48 -0300

    Cadastro de Usuário

---
+++ GoTracker/scripts/Views/Usuario.js
@@ -0,0 +1,138 @@
+﻿var model;
+var modelUrl = window.urlApi + "odata/Usuario";
+$(document).ready(function () {
+    var PagedGridModel = function (items) {
+        var self = this;
+        this.items = ko.observableArray(items);
+        this.selectedItem = ko.observable(-1);
+        this.Selected = ko.observable(null);
+        this.Cliente = ko.observable();
+        this.Clientes = ko.observableArray();
+        this.Perfis = ko.observableArray();
+        this.loadItems = function () {
+            $.ajax({
+                url: modelUrl,
+                type: 'GET',
+                error: function (xhr, status) {
+                    console.log(status);
+                },
+                success: function (result) {
+                    $(result.value).each(function () {
+                        model.items.push(ko.mapping.fromJS(this));
+                    });
+                    ko.applyBindings(model);
+                    $("div.kgViewport").css("height", "inherit");
+                    $("div.gridstyle").css("margin-top", "-25px");
+                    applyEvents(function (index) {
+                        model.selectedItem(index);
+                    });
+                }
+            });
+            $.ajax({
+                url: window.urlApi + "odata/Cliente",
+                type: 'GET',
+                error: function (xhr, status) {
+                    console.log(status);
+                },
+                success: function (result) {
+                    $(result.value).each(function () {
+                        model.Clientes.push(ko.mapping.fromJS(this));
+                    });
+                }
+            });
+            $.ajax({
+                url: window.urlApi + "odata/Perfil",
+                type: 'GET',
+                error: function (xhr, status) {
+                    console.log(status);
+                },
+                success: function (result) {
+                    $(result.value).each(function () {
+                        model.Perfis.push(ko.mapping.fromJS(this));
+                    });
+                }
+            });
+        };
+        this.addItem = function () {
+            var novoItem = ko.mapping.fromJS({ ClienteId: ko.observable(0), Nome: ko.observable(""), SobreNome: ko.observable(""), Login: ko.observable(""), Senha: ko.observable(""), Email: ko.observable(""), Email: ko.observable(""), Telefone: ko.observable(""), Celular: ko.observable(""), DataSuspensao: ko.observable(""), PerfilId: ko.observable(0) });
+            this.Selected(novoItem);
+            $('#powerwidgets').css('visibility', 'visible');
+            $('.fa-chevron-circle-up').click();
+            $('.fa-chevron-circle-down').click();
+            applyMasks();
+        };
+        this.salvar = function () {
+            var toSend = ko.utils.stringifyJson(ko.mapping.toJS(this.Selected()));
+            console.log(this.Selected());
+            console.log(toSend);
+            $.ajax({
+                url: this.Selected().Id != null ? modelUrl + "(" + model.Selected().Id() + ")"
+                                            : modelUrl,
+                type: this.Selected().Id != null ? "PUT" : "POST",
+                dataType: 'json',
+                contentType: "application/json; charset=utf-8",
+                data: toSend,
+                error: function (xhr, status) {
+                    console.log(status);
+                },
+                success: function (result) {
+                    var saved = ko.mapping.fromJS(result);
+                    if (result) {
+                        model.Selected(saved);
+                        model.Selected().Id = ko.observable(saved.Id());
+                        model.items.push(model.Selected());
+                    }
+                    if (model.Cliente())
+                        if (result)
+                            saverel(saved.Id(), model.Cliente(), "clientedosimcard", function (data) {
+                                console.log(data);
+                            });
+                        else
+                            saverel(model.Selected().Id(), model.Cliente(), "clientedosimcard", function (data) {
+                                console.log(data);
+                            });
+                    $('#powerwidgets').css('visibility', 'hidden');
+                    $('.fa-chevron-circle-up').click();
+                    applyEvents(function (index) {
+                        model.selectedItem(index);
+                    });
+                    model.items.valueHasMutated();
+                }
+            });
+        };
+        this.refresh = function (data) { console.log(data); };
+        this.gridOptions = {
+            data: self.items
+            , footerVisible: false
+            , multiSelect: false
+            , showColumnMenu: false
+            , showFilter: false
+            , displaySelectionCheckbox: false
+            , afterSelectionChange: function (grid) {
+                model.Selected(grid.selectedItems()[0]);
+                $(model.Clientes()).each(function () {
+                    if (this.Id() == model.Selected().ClienteId()) {
+                        model.Selected().ClienteId(this);
+                        return;
+                    }
+                });
+                $(model.Perfis()).each(function () {
+                    if (this.Id() == model.Selected().PerfilId()) {
+                        model.Selected().PerfilId(this);
+                        return;
+                    }
+                });
+                $('#powerwidgets').css('visibility', 'visible');
+                $('.fa-chevron-circle-down').click();
+                $('html,body').animate({ 'scrollTop': 0 }, 1000);
+                
+            }
+            , columnDefs: [
+                { field: "Nome", displayName: "Nome" },
+                { field: "SobreNome", displayName: "Sobrenome" }
+            ]
+        };
+    };
+    model = new PagedGridModel();
+    model.loadItems();
+});
---
+++ GoTrackerWS/Controllers/UsuarioController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using System.Web.Http.OData;
+using System.Web.Http.OData.Routing;
+using GoTrackerModel.Models;
+
+namespace GoTrackerWS.Controllers
+{
+    /*
+    To add a route for this controller, merge these statements into the Register method of the WebApiConfig class. Note that OData URLs are case sensitive.
+
+    using System.Web.Http.OData.Builder;
+    using GoTrackerModel.Models;
+    ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+    builder.EntitySet<Usuario>("Usuario");
+    builder.EntitySet<Perfil>("Perfils"); 
+    builder.EntitySet<Cliente>("Clientes"); 
+    config.Routes.MapODataRoute("odata", "odata", builder.GetEdmModel());
+    */
+    public class UsuarioController : ODataController
+    {
+        private GoTrackerContainer db = new GoTrackerContainer();
+
+        // GET odata/Usuario
+        [Queryable]
+        public IQueryable<Usuario> GetUsuario()
+        {
+            return db.Usuarios;
+        }
+
+        // GET odata/Usuario(5)
+        [Queryable]
+        public SingleResult<Usuario> GetUsuario([FromODataUri] int key)
+        {
+            return SingleResult.Create(db.Usuarios.Where(usuario => usuario.Id == key));
+        }
+
+        // PUT odata/Usuario(5)
+        public IHttpActionResult Put([FromODataUri] int key, Usuario usuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (key != usuario.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(usuario).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsuarioExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Updated(usuario);
+        }
+
+        // POST odata/Usuario
+        public IHttpActionResult Post(Usuario usuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Usuarios.Add(usuario);
+            db.SaveChanges();
+
+            return Created(usuario);
+        }
+
+        // PATCH odata/Usuario(5)
+        [AcceptVerbs("PATCH", "MERGE")]
+        public IHttpActionResult Patch([FromODataUri] int key, Delta<Usuario> patch)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Usuario usuario = db.Usuarios.Find(key);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            patch.Patch(usuario);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsuarioExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Updated(usuario);
+        }
+
+        // DELETE odata/Usuario(5)
+        public IHttpActionResult Delete([FromODataUri] int key)
+        {
+            Usuario usuario = db.Usuarios.Find(key);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            db.Usuarios.Remove(usuario);
+            db.SaveChanges();
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // GET odata/Usuario(5)/Perfil
+        [Queryable]
+        public SingleResult<Perfil> GetPerfil([FromODataUri] int key)
+        {
+            return SingleResult.Create(db.Usuarios.Where(m => m.Id == key).Select(m => m.Perfil));
+        }
+
+        // GET odata/Usuario(5)/Cliente
+        [Queryable]
+        public SingleResult<Cliente> GetCliente([FromODataUri] int key)
+        {
+            return SingleResult.Create(db.Usuarios.Where(m => m.Id == key).Select(m => m.Cliente));
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool UsuarioExists(int key)
+        {
+            return db.Usuarios.Count(e => e.Id == key) > 0;
+        }
+    }
+}
